72b252ccd0133746d5f2d81f0f26a5f71fc79368
Laurie1985 <berthon.laurie@neuf.fr>
2025-10-07 15:50:42 +0200

    tests unitaires

---
+++ tests/Controller/SmokeTest.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SmokeTest extends WebTestCase
+{
+    public function testApiDocUrlIsSuccessful(): void
+    {
+        $client = self::createClient();
+        $client->followRedirects(false);
+        $client->request('GET', '/api/doc');
+
+        self::assertResponseIsSuccessful();
+    }
+
+    public function testApiAccountUrlIsSecure(): void
+    {
+        $client = self::createClient();
+        $client->followRedirects(false);
+        $client->request('GET', '/api/me');
+
+        self::assertResponseStatusCodeSame(401);
+    }
+
+    public function testLoginRouteCanConnectAValidUser()
+    {
+        $client = self::createClient();
+        $client->request('POST', 'api/login', [], [], [
+            'Content-type' => 'application/json',
+        ], json_encode([
+            'username' => 'toto@toto.fr',
+            'password' => 'toto',
+        ], JSON_THROW_ON_ERROR));
+        $statusCode = $client->getResponse()->getStatusCode();
+        dd($statusCode);
+
+    }
+}
